2db830af61abc77aae2c21eb686f2dd89ac91429
xlingfairy <gruan@asnum.com>
2015-07-28 01:23:00 +0800

    Uri Image Source.

--- FlipView/MainActivity.cs
+++ FlipView/MainActivity.cs
@@ -21,24 +21,24 @@ namespace FlipView {
         protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
 
-            //var imgs = new List<string>() {
-            //    "http://photocdn.sohu.com/20150416/mp11097112_1429164315587_2.png",
-            //    "http://s1.sinaimg.cn/mw690/0024SuDBgy6RoZf6lRqe4&690",
-            //    //"http://att.x2.hiapk.com/forum/201204/03/1243289i9kxtoxihioootz.jpg",
-            //    "http://b.hiphotos.baidu.com/image/pic/item/c8ea15ce36d3d539ff0934ce3987e950352ab082.jpg"
-            //};
 
-            var imgs = new List<string>() { "A", "B", "C" };
+            var imgs = new Dictionary<string, string>(){
+                {"http://f.hiphotos.baidu.com/image/w%3D400/sign=cf3db92c0b24ab18e016e03705fbe69a/f703738da97739122e40d96bfa198618367ae252.jpg","彩虹@山"},
+                {"http://e.hiphotos.baidu.com/image/w%3D400/sign=8b347e407c3e6709be0044ff0bc69fb8/e7cd7b899e510fb32396f5f0da33c895d0430ccd.jpg","礼物"},
+                {"http://e.hiphotos.baidu.com/image/w%3D400/sign=9915223123a446237ecaa462a8237246/11385343fbf2b2116724dd05c98065380cd78e77.jpg","小鸟"}
+            };
 
             var items = imgs.Select(i => {
-                //var img = new ImageView(this);
+                var img = new ImageView(this);
                 //var client = new WebClient();
                 //client.DownloadDataCompleted += Client_DownloadDataCompleted;
-                //client.DownloadDataAsync(new Uri(i), img);
-                //return img;
-                var txt = new TextView(this);
-                txt.Text = i;
-                return txt;
+                //client.DownloadDataAsync(new Uri(i.Key), img);
+
+                var source = new UriImageSource();
+                var stm = source.GetStream(i.Key).Result;
+                var bm = BitmapFactory.DecodeStreamAsync(stm).Result;
+                img.SetImageBitmap(bm);
+                return img;
             });
 
             var vp = new ViewPager(this);
@@ -46,13 +46,13 @@ namespace FlipView {
             this.AddContentView(vp, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, 300));
         }
 
-        private void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e) {
-            var img = e.UserState as ImageView;
+        //private void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e) {
+        //    var img = e.UserState as ImageView;
 
-            img.SetImageBitmap(BitmapFactory.DecodeByteArray(e.Result, 0, e.Result.Length, new BitmapFactory.Options() {
-                InSampleSize = 4
-            }));
-        }
+        //    img.SetImageBitmap(BitmapFactory.DecodeByteArray(e.Result, 0, e.Result.Length, new BitmapFactory.Options() {
+        //        InSampleSize = 4
+        //    }));
+        //}
     }
 }
 
