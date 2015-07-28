7ceb658ec14d1c70fce773799cee188a990ee94c
gruan01 <gruan@asnum.com>
2015-07-28 18:20:00 +0800

    a

--- FlipView/MainActivity.cs
+++ FlipView/MainActivity.cs
@@ -16,43 +16,27 @@ using System.Threading.Tasks;
 namespace FlipView {
     [Activity(Label = "FlipView", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity {
-        //int count = 1;
 
-        protected override void OnCreate(Bundle bundle) {
-            base.OnCreate(bundle);
-
-
-            var imgs = new Dictionary<string, string>(){
+        private Dictionary<string, string> Imgs = new Dictionary<string, string>(){
                 {"http://f.hiphotos.baidu.com/image/w%3D400/sign=cf3db92c0b24ab18e016e03705fbe69a/f703738da97739122e40d96bfa198618367ae252.jpg","彩虹@山"},
-                {"http://e.hiphotos.baidu.com/image/w%3D400/sign=8b347e407c3e6709be0044ff0bc69fb8/e7cd7b899e510fb32396f5f0da33c895d0430ccd.jpg","礼物"},
-                {"http://e.hiphotos.baidu.com/image/w%3D400/sign=9915223123a446237ecaa462a8237246/11385343fbf2b2116724dd05c98065380cd78e77.jpg","小鸟"}
+                //{"http://e.hiphotos.baidu.com/image/w%3D400/sign=8b347e407c3e6709be0044ff0bc69fb8/e7cd7b899e510fb32396f5f0da33c895d0430ccd.jpg","礼物"},
+                //{"http://e.hiphotos.baidu.com/image/w%3D400/sign=9915223123a446237ecaa462a8237246/11385343fbf2b2116724dd05c98065380cd78e77.jpg","小鸟"}
             };
 
-            var items = imgs.Select(i => {
-                var img = new ImageView(this);
-                //var client = new WebClient();
-                //client.DownloadDataCompleted += Client_DownloadDataCompleted;
-                //client.DownloadDataAsync(new Uri(i.Key), img);
+        protected override async void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
 
-                var source = new UriImageSource();
-                var stm = source.GetStream(i.Key).Result;
-                var bm = BitmapFactory.DecodeStreamAsync(stm).Result;
-                img.SetImageBitmap(bm);
-                return img;
-            });
+            UriImageSource source = new UriImageSource();
+            var img = new ImageView(this);
+            //source.Attach(img, Imgs.First().Key);
+            img.SetImageBitmap(await BitmapFactory.DecodeStreamAsync(await source.GetStream(Imgs.First().Key)));
+
+            var items = new List<ImageView>() { img };
 
             var vp = new ViewPager(this);
             vp.Adapter = new FlipViewAdapter(items);
             this.AddContentView(vp, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, 300));
         }
-
-        //private void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e) {
-        //    var img = e.UserState as ImageView;
-
-        //    img.SetImageBitmap(BitmapFactory.DecodeByteArray(e.Result, 0, e.Result.Length, new BitmapFactory.Options() {
-        //        InSampleSize = 4
-        //    }));
-        //}
     }
 }
 
--- FlipView/UriImageSource.cs
+++ FlipView/UriImageSource.cs
@@ -16,142 +16,29 @@ using System.Net;
 using System.Security.Cryptography;
 using AO = Android.OS;
 using System.Net.Http;
+using Android.Graphics;
 
 namespace FlipView {
-    public class UriImageSource {
+    public class UriImageSource : DiskLRUCache {
 
-        /// <summary>
-        /// 是否缓存，默认开启
-        /// </summary>
-        public bool EnableCache { get; set; }
-
-        /// <summary>
-        /// 缓存时长, 默认1天
-        /// </summary>
-        public TimeSpan CacheValidity { get; set; }
-
-
-        private readonly static IsolatedStorageFile ISF = null;
-
-        private readonly static string CacheDir = "ImageCache";
-
-        static UriImageSource() {
-            ISF = IsolatedStorageFile.GetUserStoreForApplication();
-            if (!ISF.DirectoryExists(CacheDir)) {
-                ISF.CreateDirectory(CacheDir);
+        public override string SubDir {
+            get {
+                return "ImageCache";
             }
         }
 
-        public UriImageSource() {
-            this.CacheValidity = TimeSpan.FromDays(1);
-            this.EnableCache = true;
+        public async Task<ImageView> Get(Context ctx, string url) {
+            var stm = await this.GetStream(url);
+            var bm = await BitmapFactory.DecodeStreamAsync(stm);
+            var img = new ImageView(ctx);
+            img.SetImageBitmap(bm);
+            return img;
         }
 
-        public async Task<Stream> GetStream(string url) {
-            var uri = new Uri(url);
-            return await this.GetStream(uri);
-        }
-
-        /// <summary>
-        /// 获取文件流
-        /// </summary>
-        /// <param name="uri"></param>
-        /// <returns></returns>
-        public async Task<Stream> GetStream(Uri uri) {
-            Stream stm = null;
-            if (!this.EnableCache) {
-                stm = await this.GetStreamAsync(uri).ConfigureAwait(false);
-            } else {
-                var key = MD5(uri.AbsoluteUri);
-                stm = await this.GetStreamFromCache(key).ConfigureAwait(false);
-                if (stm == null) {
-                    stm = await this.GetStreamAsync(uri).ConfigureAwait(false);
-                    if (stm != null) {
-                        await this.SaveCache(key, stm);
-                    }
-                }
-            }
-
-            return stm;
-        }
-
-        /// <summary>
-        /// 从网络获取文件流
-        /// </summary>
-        /// <param name="uri"></param>
-        /// <returns></returns>
-        private async Task<Stream> GetStreamAsync(System.Uri uri) {
-            Stream stream = null;
-            using (var client = new HttpClient())
-            using (var rep = await client.GetAsync(uri)) {
-                //WebClient
-                //var bytes = await client.DownloadDataTaskAsync(uri);
-                //stream = new MemoryStream(bytes);
-                stream = await rep.Content.ReadAsStreamAsync();
-            }
-            return stream;
-        }
-
-        //public async Task<Stream> GetStreamAsync(System.Uri uri, CancellationToken cancellationToken) {
-        //    Stream stream;
-        //    using (HttpClient httpClient = new HttpClient()) {
-        //        using (HttpResponseMessage async = await httpClient.GetAsync(uri, cancellationToken))
-        //            stream = await async.Content.ReadAsStreamAsync();
-        //    }
-        //    return stream;
-        //}
-
-        /// <summary>
-        /// 从缓存获取文件流
-        /// </summary>
-        /// <param name="uri"></param>
-        /// <returns></returns>
-        private async Task<Stream> GetStreamFromCache(string key) {
-            Stream stm = null;
-            var path = Path.Combine(CacheDir, key);
-            if (!await this.IsExpire(path)) {
-                stm = ISF.OpenFile(path, FileMode.Open, FileAccess.Read);
-            }
-            return stm;
-        }
-
-        /// <summary>
-        /// 是否过期, 如果不存在，按过期处理
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        private Task<bool> IsExpire(string path) {
-            //如果文件不存在， 直接是过期处理
-            if (!ISF.FileExists(path))
-                return Task.FromResult(true);
-
-            var offset = ISF.GetLastWriteTime(path);
-            return Task.FromResult(DateTime.Now - offset > this.CacheValidity);
-        }
-
-        /// <summary>
-        /// 保存缓存
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="stm"></param>
-        /// <returns></returns>
-        private Task SaveCache(string key, Stream stm) {
-            var path = Path.Combine(CacheDir, key);
-            using (var f = ISF.OpenFile(path, FileMode.Create, FileAccess.Write)) {
-                return stm.CopyToAsync(f);
-            }
-        }
-
-        private static string MD5(string input) {
-            using (var md5Hasher = System.Security.Cryptography.MD5.Create()) {
-                byte[] data = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(input));
-                StringBuilder sBuilder = new StringBuilder();
-
-                for (int i = 0; i < data.Length; i++) {
-                    sBuilder.Append(data[i].ToString("x2"));
-                }
-                return sBuilder.ToString();
-            }
+        public async void Attach(ImageView img, string url) {
+            var stm = await this.GetStream(url);
+            var bm = await BitmapFactory.DecodeStreamAsync(stm);
+            img.SetImageBitmap(bm);
         }
     }
 }
